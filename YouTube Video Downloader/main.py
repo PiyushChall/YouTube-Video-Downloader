cb067ebcbf19976ae82418f42739dcd94e5d6f38
PiyushChall <challpiyush79@gmail.com>
2024-04-25 12:22:49 +0530

    some more improvements

--- YouTube Video Downloader/main.py
+++ YouTube Video Downloader/main.py
@@ -1,3 +1,4 @@
+# Importing Modules
 import tkinter
 import customtkinter
 from pytube import YouTube
@@ -9,12 +10,12 @@ def start_download():
         youtube_object = YouTube(youtube_link, on_progress_callback=on_progress)
         video_title = youtube_object.title
         video = youtube_object.streams.get_highest_resolution()
-        title.configure(text=video_title,text_color="white")
+        title.configure(text=video_title, text_color="white")
         finish_download.configure(text="")
         video.download()
-        finish_download.configure(text="Downloaded",text_color="green")
+        finish_download.configure(text="Downloaded", text_color="green")
     except:
-        finish_download.configure(text="INVALID LINK or Download Error", text_color="red")
+        finish_download.configure(text="OOPS INVALID LINK or Download Error", text_color="red")
 
 
 def on_progress(stream, chunk, bytes_remaining):
@@ -23,33 +24,41 @@ def on_progress(stream, chunk, bytes_remaining):
     percentage_of_completion = bytes_downloaded / total_size * 100
     completion_percentage = str(int(percentage_of_completion))
     progress_percentage.configure(text=completion_percentage + "%")
+    progress_percentage.update()
+    progress_bar.set(float(percentage_of_completion) / 100)
 
 
+# Appearance
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("green")
-
 app = customtkinter.CTk()
 app.geometry("720x480")
-app.title("|YouTube Video Downloader|")
+app.title("YouTube Video Downloader")
 
 title = customtkinter.CTkLabel(app, text=" Just Insert that link here ")
 title.pack(padx=10, pady=10)
 
+# Url Insertion
 url = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=50, textvariable=url)
 link.pack()
 
+# Download Completion
 finish_download = customtkinter.CTkLabel(app, text="")
 finish_download.pack()
 
+# Progress Percentage
 progress_percentage = customtkinter.CTkLabel(app, text="")
 progress_percentage.pack()
 
+# Progress Bar
 progress_bar = customtkinter.CTkProgressBar(app, width=400)
 progress_bar.set(0)
 progress_bar.pack(padx=10, pady=10)
 
+# Download Button
 download = customtkinter.CTkButton(app, text="<Download>", command=start_download)
 download.pack(padx=10, pady=10)
 
+# MainLoop of the app
 app.mainloop()
